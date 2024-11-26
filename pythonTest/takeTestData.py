61c2bb5427f6806d75868e493c7db3da29981dee
Duyle <98381388+Duyzzz@users.noreply.github.com>
2024-11-27 00:10:54 +0700

    remove secrect imformation

--- pythonTest/takeTestData.py
+++
@@ -1,6 +0,0 @@
-import face_recognition
-path = "C:\\Users\\DELL\\Pictures\\Camera Roll\\WIN_20231214_00_16_49_Pro.jpg"
-
-loadFile = face_recognition.load_image_file(path)
-encoder = face_recognition.face_encodings(loadFile)
-print(encoder)
